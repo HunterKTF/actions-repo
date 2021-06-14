75e5b1ff7c20ed46999210541d2514c2c3ceebd6
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2021-06-14 00:31:15 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 rm -f /tmp/.X1-lock /tmp/.X11-unix/X1
 
 # Start virtual screen
-/sbin/start-stop-daemon --start --pidfile /tmp/custom_xvfb_1.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :1 -ac -screen 0 1280x1024x16;
+sudo /sbin/start-stop-daemon --start --pidfile /tmp/custom_xvfb_1.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :1 -ac -screen 0 1280x1024x16;
 sleep 5;
 
 export EAGLE_DIR=/opt/eagle-9.6.2;
@@ -13,20 +13,20 @@ export EAGLE_USER="rhernandez@valiot.io"
 export EAGLE_PSW="DeltaEcho9"
 
 # Eagle Auto Login.
-${EAGLE_DIR}/eagle & sleep 650;
-xdotool key Left;
+sudo ${EAGLE_DIR}/eagle & sleep 40;
+sudo xdotool key Left;
 sleep 15;
-xdotool key Return;
+sudo xdotool key Return;
 sleep 15;
-xdotool type ${EAGLE_USER};
+sudo xdotool type ${EAGLE_USER};
 sleep 15;
-xdotool key Return;
+sudo xdotool key Return;
 sleep 15;
-xdotool type ${EAGLE_PSW};
+sudo xdotool type ${EAGLE_PSW};
 sleep 15;
-xdotool key Return;
+sudo xdotool key Return;
 sleep 10;
-kill $(pidof ${EAGLE_DIR}/eagle);
+sudo kill $(pidof ${EAGLE_DIR}/eagle);
 sleep 12;
 
 # Execute CAM Jobs
