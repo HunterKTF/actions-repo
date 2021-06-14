f4295cbfbf034b222a9536aa9bae501584655d75
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2021-06-14 00:15:48 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ export EAGLE_USER="rhernandez@valiot.io"
 export EAGLE_PSW="DeltaEcho9"
 
 # Eagle Auto Login.
-${EAGLE_DIR}/eagle & sleep 300;
+${EAGLE_DIR}/eagle & sleep 650;
 xdotool key Left;
 sleep 15;
 xdotool key Return;
