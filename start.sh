85ad9b50c0c72f15d921c4829e94a5ce744677bb
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2021-06-14 16:36:24 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -34,6 +34,4 @@ sudo ${EAGLE_DIR}/eagle -X -c+ -N -dCAMJOB -j/home/user/ELECROW_gerber_v9.cam -o
 sleep 3
 
 # Verify outputs
-ls /home/user/bin/CAM/;
-ls -ls /home/user/bin/CAM/Assembly;
-ls -ls /home/user/bin/CAM/Gerbers;
+sudo ls /home/user/bin/
