40ada45936ff1069745ecb001c971bd0e76927b2
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2021-06-14 10:14:10 -0500

    new creds file for python app

---
+++ creds.js
@@ -0,0 +1,12 @@
+{
+  "type": "service_account",
+  "project_id": "tetsproject-310902",
+  "private_key_id": "b9a19d73289e811bd4866af28e8bf748c111a499",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQC2zyx5B8dcWRYF\nyvALDmwZaqPgoyyWgsPwLffU+apqSiOjizMCkPON4S0jTrh7itnnPaIzyxMZjNIW\n/AXru0W/X9SJWfcIT+ND1Tu8czbm4r9gyJRVr+xgeuVkhhpCPw8qULZHnNuS0ZJa\nMT1dLWqRXCfRNG/RkLVskCZJ8at5Syf498RNvqB2r/guFx8wUt1dmadBPlV9xJLc\nUVs6Wl17EyRGs4PVG04Sg8siol0/h282T51RiWYHQu9hEbyRIsECM9eYNcUpTbB/\nUp8My2ghCrB0TqcmHtO/JrDrbeeWYXpabw5VykiP8CtlYp6zLfu6iGykbDAES5Mj\nKKH2lRLtAgMBAAECggEAGZgc4l8P7uJ8HliAzhN5V/+hrQg7bGEnfJ969ML1tIbn\nwXRyMlhH+SO/FZPmaUmR43myFW1Hocqg+V3UmGHyTG5my8ogN3Yz2+HlSKea1q2F\nXNAIUIzeW82pAy9xSrgYb/S36IPSDBayVlVRgZOjuXE9E+GzDBSmCeqOqcifmM4L\nlH9855tHu9Hr29A65FinE9Q8qgT7+vUfp59D8iqJ4zdVzMqtRglBt1j1EavsBYQg\nJS0cw1dLApAHQk+jTGraqYabuv2mWWf7U9I+IirBwIlnKRIbl1AfM19i4AoBf6nX\ncdu68odA0pAd8JOMfvDQhlWPwoG2pMedFjvDT/SZAwKBgQDtw6N0wT+w6I26idjB\nu5K3Cwvmm5Us7K+2oDpjRFQggUtzNzTapfqdD3gev8lryBmFfzwqakRPv6db7C3d\ndWhtp9h/gdi9oqk0koLmkQBqWybkN7BAc2kEawyB5QeHacEE/gIY1/SFeXT47noD\nsotOaGwUAxa8E1v8hSAnFOZxFwKBgQDE1IbsvKkjueQku+L/4ENzd7G+hP/NmWEC\n4kRDUmcpojVeG/Bhiz1vjf+mkLdGqX4TpBi1sr7uoyXXYnNqe6/cWOJuPMytYNQI\npWmQi/NIHo2NXS9aZW8jR+vctP1p0rjlathhgCxMnSwLLOOjHy1/ElselOCEp1do\nzyRZ6UJ2mwKBgE7ZgI2pwNuEY+wm5tFoyUEW0r2JfzFE1Qj1nidpc5TMDB+LgWW5\ngaEwjh90bvHljEEvbt6KG1lvL6tWaed1lko3TZ8LirtpBEBBDlQ0ZjTfQciRh0v4\nei1S6vTw5UxNEAv9yOhARgE1xEsBm0k6nuRD0QPZ+mIo8WqPI5iSX9HFAoGBALO+\ngRhfkPYXI8iH69xPKEmh3KdnPVK75mODzWLNNXhHsQSn7vSk+w2MVxH8xeG5lnDJ\nQedrvACg7EWsmkl7ejV21hBlZWRPMFJU+/oMU7ZgcFPP7RZX2w4U98ctYN3qmP6o\ne8FOEyZDwZzS+W5VYm7QjHV47y80whXsU/3njdJRAn95Imilwp1hIpBEsz5koI9r\nXGEEvJXBSfSdNN4lSed5SVx1mbg+TBvVvjTwppCpasvqpVwtuzriLmrNHKJ1ai+N\nWXdqIfeY6ADwsfAQVtyvYVCumqqRWuLVFdS4R5IsDn1KuDoUGgElPmuL0HAal3En\ne7iXylKUSpmpjkeLrl0m\n-----END PRIVATE KEY-----\n",
+  "client_email": "hunter@tetsproject-310902.iam.gserviceaccount.com",
+  "client_id": "105690886942703943628",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/hunter%40tetsproject-310902.iam.gserviceaccount.com"
+}
