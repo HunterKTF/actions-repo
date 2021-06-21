68e0635772cf5e746bf835d101380a0feacb9c64
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2021-06-20 22:43:09 -0500

    Update boms-auto.py

--- boms-auto.py
+++ boms-auto.py
@@ -34,7 +34,7 @@ def share_with():
 # Function to get credentials to create a spreadsheet
 def getCredentials():
     scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("/creds.json", scope)
     time.sleep(10)
     return gspread.authorize(creds)
 
