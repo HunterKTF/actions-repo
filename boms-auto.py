531c1e692e707b062b2c422b84bfbd4e26f366b2
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2021-07-16 18:03:10 -0500

    Update boms-auto.py

--- boms-auto.py
+++ boms-auto.py
@@ -1,11 +1,13 @@
 '''
 Boms Automation V1.0.0
-
 Made by: Jorge R. Hernández Sabino
-Updated: 06-17-21
+Updated: 06-26-21
 Contact: jorgehernandez336@gmail.com
 '''
 
+#!/usr/bin/env python3
+
+# Import necessary modules
 import os
 import time
 import csv
@@ -17,11 +19,13 @@ from pprint import pprint
 
 # Function to retrieve file contents
 def getFiles():
+    path = '/Users/jorgehernandez/github/mikroBUS-Valiot/Acelerometro_PCB/sourcing/CAM/Assembly/'
     pnp_files = []
-    files = os.listdir()
+    files = os.listdir(path)
     for x in files:
         if 'PnP' in x:
-            pnp_files.append(x)
+            str_out = path + x
+            pnp_files.append(str_out)
     return pnp_files
 
 # Function to define users to share spreadsheet
@@ -33,22 +37,42 @@ def share_with():
 
 # Function to get credentials to create a spreadsheet
 def getCredentials():
-    scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name('/creds.json', scope)
+    scope = [
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive'
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
     time.sleep(10)
     return gspread.authorize(creds)
 
 def create_sheet():
+    sheets_list = []
+    spreadsheet_files = []
     users = share_with()
     gc = getCredentials()
     files = getFiles()
-    title = files[0][4:-9]
-    sh = gc.open(title)
-    print(gspread.exceptions.SpreadsheetNotFound)
-    # if !sh:
-    sh = gc.create(title)
-    sh.share(users, perm_type='user', role='writer')
-    worksheet = sh.add_worksheet(title=title, rows="100", cols="20")
+    title = "final_test3" # files[0][4:-9]
+    spreadsheets_files = gc.list_spreadsheet_files()
+
+    for sheets in spreadsheets_files:
+        sheets_list.append(sheets["name"])
+
+    print(sheets_list)
+    if title not in sheets_list:
+        sh = gc.create(title)
+        sh.share(users, perm_type='user', role='writer')
+        ws_title = title + "-1"
+        worksheet = sh.add_worksheet(title=ws_title, rows="100", cols="20")
+        ws = sh.sheet1
+        sh.del_worksheet(ws)
+        url = sh.url
+        url += "/edit#gid=0"
+    else:
+        sh = gc.open(title)
+        worksheet_list = sh.worksheets()
+        list_len = len(worksheet_list)
+        ws_title = title + "-" + str(list_len+1)
+        worksheet = sh.add_worksheet(title=ws_title, rows="100", cols="20")
     time.sleep(10)
     return sh, worksheet
 
@@ -65,7 +89,7 @@ def create_titles(sh, worksheet):
     worksheet.update_cell(1,10,"Precio")
     worksheet.update_cell(1,11,"Subtotal")
     worksheet.update_cell(1,12,"Total")
-    time.sleep(30)
+    time.sleep(20)
 
 def init_api():
     headers = {
@@ -78,12 +102,11 @@ def init_api():
     time.sleep(10)
     return headers, params
 
-os.chdir("/home/user/bin/CAM/")
-csvFiles = getFiles()
+csvfiles = getFiles()
 dict_qty = {}
 dict_val = {}
 cc=0
-for file in csvFiles:
+for file in csvfiles:
     line_count=0
     with open(file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -97,11 +120,7 @@ for file in csvFiles:
                 dict_val[data_qty]+=", "+row[0]
 
 sh, worksheet = create_sheet()
-ws = sh.sheet1
-sh.del_worksheet(ws)
 create_titles(sh, worksheet)
-url = sh.url
-url += "/edit#gid=0"
 headers, params = init_api()
 qString1 = '{ "SearchByPartRequest": { "mouserPartNumber": "'
 qString2 = '", "partSearchOptions": "string" }}'
@@ -116,7 +135,7 @@ for key in dict_qty:
         mpn = value.strip(' "')
         codigo_valiot = file[4:file.find("_front.csv")] + '-' + str(cc)
         qty = dict_qty[key]
-        concept = "pruebas"
+        concept = "Desarrollo"
         parts = dict_val[key]
         mpn = value
         data = qString1 + mpn + qString2
@@ -138,4 +157,3 @@ for key in dict_qty:
         cc+=1
 
 worksheet.update_cell(2,12,'=sum(K2:K' + str(cc+2) + ')')
-print(url)
